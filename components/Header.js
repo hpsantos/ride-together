0fdb0b248f2f9848d3fa9f362879b21d0ef5bd0c
Celso Marques <djmarkcell@gmail.com>
2021-11-20 17:17:34 +0000

    Cleanup initial dummy things and small style adjustments (#15)

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
   }
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" fixed="top">
       <Container>
         <Link href="/" passHref>
           <Navbar.Brand href="/">Ride Together</Navbar.Brand>
@@ -31,18 +31,13 @@ const Header = () => {
                 </Nav.Link>
               </Link>
             )}
-            <Link href="/about" passHref>
-              <Nav.Link href="/about" active={getActiveClass('/about')}>
-                About
-              </Nav.Link>
-            </Link>
           </Nav>
         </Navbar.Collapse>
 
         {user && (
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              Signed in as: <strong>{user.name}</strong>
+              Signed in as <strong>{user.name}</strong>
             </Navbar.Text>
             <Nav className="ml-3">
               <Nav.Link
--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Container from 'react-bootstrap/Container'
 
 import Header from '~components/Header'
@@ -5,6 +6,11 @@ import Header from '~components/Header'
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Ride Together</title>
+        <meta name="description" content="..." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Container className="pt-3">{children}</Container>
     </>
