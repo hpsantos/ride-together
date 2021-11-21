e8595fdb2b486f391e0cecccd7fd66c10861e55f
Celso Marques <djmarkcell@gmail.com>
2021-11-21 03:05:32 +0000

    Add range input (#35)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react'
-import { Button, Col, Row } from 'react-bootstrap'
+import { Button, Col, Form, Row } from 'react-bootstrap'
 
 import { Map } from '~components/map/Map'
 import { SelectTime } from '~components/SelectTime'
@@ -58,20 +58,31 @@ export default function Home() {
     <>
       <div className="text-center">
         <h2>
-          Welcome to Ride<strong>Together!</strong>
+          Welcome to Ride<strong>Together</strong>!
         </h2>
         <p className="lead text-muted">
           Get started by searching for routes below
         </p>
       </div>
-      <Row className="justify-content-center my-3">
-        <Col xs="auto" sm="3">
-          <SelectTime onHoursChange={setHours} onMinutesChange={setMinutes} />
+      <Row className="align-items-center mt-5 mb-3">
+        <Col xs="10">
+          <Row className="align-items-end">
+            <Col xs="6">
+              <SelectTime
+                onHoursChange={setHours}
+                onMinutesChange={setMinutes}
+              />
+            </Col>
+            <Col xs="auto">
+              <Button onClick={searchRoutes} className="px-5">
+                <i className="bi-search"></i> &nbsp;Search
+              </Button>
+            </Col>
+          </Row>
         </Col>
-        <Col xs="auto">
-          <Button onClick={searchRoutes} className="px-5">
-            <i className="bi-search"></i> &nbsp;Search
-          </Button>
+        <Col xs="2">
+          <Form.Label className="small mb-0">Distance radius</Form.Label>
+          <Form.Range min="0" max="25" />
         </Col>
       </Row>
       <Map routes={routes} onMapClick={handleMapClick} />
